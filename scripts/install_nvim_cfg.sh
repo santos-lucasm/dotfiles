7a21bbc20baed7529bb469e71191eb92ebed6407
santos-lucasm <santos.lucasmmatheus@gmail.com>
2024-03-29 00:48:25 -0300

    Modify install script to symlink all files from folders automatically

--- scripts/install_nvim_cfg.sh
+++ scripts/install_nvim_cfg.sh
@@ -1,27 +1,26 @@
+#!/bin/bash
+
 GIT_CFG="${HOME}/ws/personal-cfg"
-LOCAL_CFG="${HOME}/.config"
 
 symlink_file () {
-    ln -s "${GIT_CFG}/$1" "${LOCAL_CFG}/$1"
+    ln -s $1 $2
+}
+
+install_files_from() {
+    for filename in ${GIT_CFG}/$1/*.lua; do
+        symlink_file "${filename}" "${HOME}/.config/$1/$(basename "$filename")"
+    done
 }
 
 rm -rf ~/.config/nvim
 
 mkdir ~/.config/nvim
-symlink_file "nvim/init.lua"
+install_files_from "nvim"
 
 # Lua profile
 mkdir -p ~/.config/nvim/lua/santos
-symlink_file "nvim/lua/santos/init.lua"
-symlink_file "nvim/lua/santos/packer.lua"
-symlink_file "nvim/lua/santos/remap.lua"
-symlink_file "nvim/lua/santos/set.lua"
+install_files_from "nvim/lua/santos"
 
 # Nvim plugins cfg
 mkdir -p ~/.config/nvim/after/plugin
-symlink_file "nvim/after/plugin/telescope.lua"
-symlink_file "nvim/after/plugin/treesitter.lua"
-
-mkdir -p ~/.config/nvim/plugin
-symlink_file "nvim/plugin/colors.lua"
-symlink_file "nvim/plugin/plugins.lua"
+install_files_from "nvim/after/plugin"
