41b27f51777f54cec6d66bf167e11c06b73bee87
santos-lucasm <lucasdos-santos@siemens.com>
2025-06-05 10:34:26 -0300

    Update neovim configuration install script

--- .config/nvim/scripts/install_nvim_container.sh
+++ .config/nvim/scripts/install_nvim_container.sh
@@ -6,21 +6,23 @@ if command -v nvim >/dev/null 2>&1; then
 fi
 
 echo "Downloading nvim..."
-cd /home/vscode
-mkdir neovim
-cd neovim
+cd $HOME
+mkdir neovim && cd neovim
 wget https://github.com/neovim/neovim/releases/download/v0.11.2/nvim-linux-x86_64.appimage > /dev/null 2>&1
 
 echo "Installing nvim..."
 chmod u+x nvim-linux-x86_64.appimage
 ./nvim-linux-x86_64.appimage --appimage-extract
-sudo ln -s /home/vscode/neovim/squashfs-root/usr/bin/nvim /usr/bin/nvim
+sudo ln -s $HOME/neovim/squashfs-root/usr/bin/nvim /usr/bin/nvim
 
 echo "Cloning nvim packer repo..."
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim > /dev/null 2>&1
 
+echo "Creating nvim plugins configuration files symbolic links"
+ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
+
 echo "Installing nvim plugins..."
-nvim --headless -u ~/.config/nvim/lua/santos/packer.lua -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
+nvim --headless -u $HOME/.config/nvim/lua/santos/packer.lua -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
 
 
