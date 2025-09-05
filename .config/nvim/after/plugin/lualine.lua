e1a370ed15e4d6a2aeee8ac1211f4c84925c1b56
santos-lucasm <santos.lucasmmatheus@gmail.com>
2025-09-05 09:35:47 -0300

    Update lualine tabs lenght

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -11,7 +11,7 @@ require('lualine').setup {
     options = { theme = 'tokyonight-night', showtabline = 2 },
     globalstatus = true,
     tabline = {
-        lualine_a = { { 'tabs', mode = 2, path = 0 } },
+        lualine_a = { { 'tabs', mode = 2, path = 0, tab_max_length = 60, max_lenght = 120 } },
         lualine_z = { { 'windows', mode = 1, disabled_buftypes = { 'DiffviewFilePanel' } } },
     },
     sections = {
