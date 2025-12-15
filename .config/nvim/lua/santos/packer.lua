83cfb3a9d2b96a43be2fb1bc103ab75b4de8ae4c
santos-lucasm <santos.lucasmmatheus@gmail.com>
2025-12-15 06:55:12 -0300

    update packer plugin to use master

--- .config/nvim/lua/santos/packer.lua
+++ .config/nvim/lua/santos/packer.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-    use { 'nvim-telescope/telescope.nvim', tag = '0.1.5', requires = { { 'nvim-lua/plenary.nvim' } } }
+    use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/plenary.nvim' } } }
 
     -- Color themes related
     -- use { "xiyaowong/transparent.nvim" } -- transparent on all windows
