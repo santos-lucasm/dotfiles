b522b3c1f98790ae284624c085276cc541c24ed5
santos-lucasm <santos.lucasmmatheus@gmail.com>
2025-12-18 06:30:02 -0300

    Update treesiter, .configs removed from its main branch

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter'.setup {
   ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc"},
   sync_install = false,
   auto_install = true,
